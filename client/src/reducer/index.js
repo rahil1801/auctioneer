8eab8375cefd2d8acf93da5e675e79a2f61c0d62
Rahil Alam <130227036+rahil1801@users.noreply.github.com>
2025-08-09 13:49:35 +0530

    Updating Directories

--- client/src/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import profileReducer from '../slices/profileSlice';
-
-const rootReducer = combineReducers({
-    profile: profileReducer
-});
-
-export default rootReducer;
